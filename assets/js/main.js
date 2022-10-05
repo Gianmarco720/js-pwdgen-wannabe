a4a99e27c4ad6f2cb27b1797b9dad23aa6367492
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-05 13:59:14 +0200

    end pwdgen

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,18 @@ const userSurname = prompt('Qual è il tuo cognome?')
 // Chiedi il colore preferito all'utente
 const userColor = prompt('Qual è il tuo colore preferito?')
 
+// Anno corrente
+const currentYear = '22'
+
+// Calcolo password dell'utente
+const userPwd = userName + userSurname + userColor + currentYear
+
+// Stampa il risultato su pagina
+document.getElementById("password_text").innerHTML = 'Ecco la tua insicurissima password:'
+document.getElementById("password").innerHTML = userPwd
+
 // Output console
-console.log(userName);
-console.log(userSurname);
-console.log(userColor);
+console.log("Il tuo nome è:" + " " + userName);
+console.log(`Il tuo cognome è: ${userSurname}`);
+console.log(`Il tuo colore preferito è: ${userColor}`);
+console.log(`La tua password è: ${userPwd}`);
