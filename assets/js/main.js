50ef8440a6e43832d58d0c51da9250ec876e2b16
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-05 13:28:57 +0200

    add user info

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// Chiedi il nome all'utente
+const userName = prompt('Come ti chiami?')
+
+// Chiedi il cognome all'utente
+const userSurname = prompt('Qual è il tuo cognome?')
+
+// Chiedi il colore preferito all'utente
+const userColor = prompt('Qual è il tuo colore preferito?')
+
+// Output console
+console.log(userName);
+console.log(userSurname);
+console.log(userColor);
