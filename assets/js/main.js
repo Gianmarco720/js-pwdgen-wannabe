6bfe2780c3c61d542ae596b0a429bb8eb4bffe60
Gianmarco720 <orabonagianmarco379@gmail.com>
2022-10-06 14:52:42 +0200

    add ;

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,21 @@
 // Chiedi il nome all'utente
-const userName = prompt('Come ti chiami?')
+const userName = prompt('Come ti chiami?');
 
 // Chiedi il cognome all'utente
-const userSurname = prompt('Qual è il tuo cognome?')
+const userSurname = prompt('Qual è il tuo cognome?');
 
 // Chiedi il colore preferito all'utente
-const userColor = prompt('Qual è il tuo colore preferito?')
+const userColor = prompt('Qual è il tuo colore preferito?');
 
 // Anno corrente
-const currentYear = '22'
+const currentYear = '22';
 
 // Calcolo password dell'utente
-const userPwd = userName + userSurname + userColor + currentYear
+const userPwd = userName + userSurname + userColor + currentYear;
 
 // Stampa il risultato su pagina
-document.getElementById("password_text").innerHTML = 'Ecco la tua insicurissima password:'
-document.getElementById("password").innerHTML = userPwd
+document.getElementById("password_text").innerHTML = 'Ecco la tua insicurissima password:';
+document.getElementById("password").innerHTML = userPwd;
 
 // Output console
 console.log("Il tuo nome è:" + " " + userName);
